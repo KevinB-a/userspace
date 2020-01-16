c86810e8eb2582ccc1018653d23a3f1cdd2c5074
Kevin <kevin.billet.pro@gmail.com>
2020-01-16 14:34:23 +0100

    add several methods

---
+++ model/usermodel.py
@@ -0,0 +1,37 @@
+from connection import Connection
+
+class UserModel():
+    """ """
+    def __init__(self):
+        self.db = Connection()
+    
+    def log_in(self, pseudo):
+        """ """
+        self.db.initialize_connection()
+        self.db.cursor.execute("SELECT * FROM users WHERE pseudo = %s;",(pseudo))
+        view=self.db.cursor.fetchone()
+        self.db.close_connection()
+        return view
+        
+    def create_an_account(self,lastname, firstname, pseudo, email, age, password):
+        """ """
+        self.db.initialize_connection()
+        self.db.cursor.execute(
+        "INSERT INTO users(lastname, firstname, pseudo, email, age, password) VALUES (%s,%s,%s,%s,%s,%s);",
+        (lastname, firstname, pseudo, email, age, password))
+        self.db.connection.commit() 
+        self.db.close_connection() 
+        
+    def delete_an_account(self,pseudo):
+        """ """
+        self.db.initialize_connection()
+        self.db.cursor.execute("DELETE FROM users WHERE pseudo = %s;"(pseudo))
+        self.db.commit()
+        self.db.close_connection()
+    
+    def update_an_account(self,column, value, pseudo):
+        """ """
+        self.db.initialize_connection()
+        self.db.cursor.execute("UPDATE users set %s= %s WHERE pseudo = %s;"(column, value, pseudo))
+        self.db.commit()
+        self.db.close_connection()    
---
+++ view/userview.py
@@ -0,0 +1,33 @@
+from model.usermodel import UserModel
+
+class UserView():
+    """ """
+    def __init__(self):
+        self.usermodel = UserModel
+    
+    def  to_log_in(self):
+        pseudo = input("veuillez saisir votre pseudo")
+        self.usermodel.log_in(pseudo)
+    
+        
+    def new_account(self):
+        """ """
+        lastname = input("veuillez saisir votre nom de famille")
+        firstname = input("veuillez saisir votre prenom")
+        pseudo = input("veuillez saisir votre pseudo")
+        email = input("veuillez saisir votre email")
+        age = int(input("veullez saisir votre age"))
+        password = input("veuillez saisir votre mot de passe")
+        self.usermodel.create_an_account(lastname, firstname, pseudo, email, age, password)
+        
+    def to_delete_an_account(self):
+        """ """
+        pseudo = input("veuillez saisir votre pseudo")
+        self.usermodel.delete_an_account(pseudo)
+    
+    def to_update_an_account(self):
+        """ """
+        column = input("veuillez saisir le champ de la table")
+        value = input("veuillez saisir la nouvelle valeur")
+        pseudo = input("veuillez saisir votre pseudo")
+        self.usermodel.update_an_account(column, value, pseudo)
